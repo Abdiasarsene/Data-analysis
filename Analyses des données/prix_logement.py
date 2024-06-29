c931095cd8068be11738686a2bcd8ad9bff66453
Abdiasarsene <abdiasarsene@gmail.com>
2024-06-28 23:08:08 -1100

    création d'une nouvelle base de données après perte d'une plus grande base de données

---
+++ Analyses des données/prix_logement.py
@@ -0,0 +1,6 @@
+# importation des librairies
+import pandas as pd
+import missingno as msno
+
+# importation de la base de données
+prixlogement=pd.read_excel(r'donne_immobilier.xlsx')
---
+++ Bases de données/essai.sql
@@ -0,0 +1,18 @@
+create TABLE personaldata (
+    `nom` varchar(100),
+    `prenom` varchar (100),
+    `age` int,
+    `cptebankk` int 
+);
+INSERT INTO `personaldata` (
+    nom,
+    prenom,
+    age,
+    cptebank
+)
+VALUES(
+    'abdias',
+    'arsene',
+    22,
+    2000
+);
