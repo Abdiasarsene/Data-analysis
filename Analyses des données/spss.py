691fb6876bc66865607afbe9cff120ba042f9883
Abdiasarsene <abdiasarsene@gmail.com>
2024-07-17 12:54:45 -1100

    suite de l'analyse

--- Analyses des données/spss.py
+++ Analyses des données/spss.py
@@ -8,6 +8,7 @@ from scipy.stats import pearsonr
 from statsmodels.stats.diagnostic import het_breuschpagan
 from scipy.stats import shapiro
 from statsmodels.stats.stattools import durbin_wtason
+from sklearn.preprocessing import MinMaxScaler
 
 spss=pd.read_excel(r'c:\Users\ARMIDE Informatique\Desktop\20220609 - (GROUP)-1.xlsx')
 print(spss.columns)
@@ -40,12 +41,10 @@ datasoi =spss[['MALE', 'AGE_p.6','scoreTotalConflit','scoreTotalImageSoi','score
 
 # vérification des doublons, des valeurs abérrantes et manquantes
 
-# valeurs manquantes
-msno.bar(datasoi, color='blue')
-
 # doublons
-doublons=datasoi.duplicated().sum()
-print("les doublons sont : ",doublons)
+doublons=datasoi[datasoi.duplicated()]
+print("les doublons sont : ")
+doublons
 
 # détection des outliers avec la méthode iqr
 Q1 = datasoi.quantile(0.25)
@@ -58,8 +57,48 @@ valeurs_aberrantes=datasoi[outliers.any(axis=1)]
 print("les valeurs aberrantes sont : ")
 valeurs_aberrantes
 
+# suppression des doublons
+datasoi= datasoi.duplicates()
+
 # suppression des lignes contenant des outliers
 print('Nouvelle base de données sans les outliers :')
 datasoi=datasoi.drop(valeurs_aberrantes.index)
 datasoi
 
+# valeurs manquantes
+msno.bar(datasoi, color='blue')
+
+# normalisation des données
+minmaxscaler =MinMaxScaler()
+datasoinorma=datasoi.copy()
+datasoinorma[datasoi.columns]=minmaxscaler.fit_transform(datasoi[datasoi.columns])
+datasoinorma
+
+# statistique descriptive
+statistic= datasoinorma.describe()
+
+# matrice de corrélations
+correation =datasoinorma.corr()
+sns.heatmap(correation, annot=True, cmap='coolwarm')
+plt.show()
+
+# Rélation entre la variabe cible et les préditeurs
+variables = ['MALE', 'AGE_p.6', 'scoreTotalConflit','scoreTotalAnxiety']
+fig, ax = plt.subplots(2,2, figsize=(18, 9))
+# Couleurs pour chaque variable
+colors = ['red', 'purple', 'green', 'black', 'orange']
+
+# Tracé des graphiques
+for ax, var, color in zip(ax.flat, variables, colors):
+    sns.regplot(x='scoreTotalImageSoi', y=var, data=datasoinorma, scatter_kws={'s': 50}, line_kws={'color': color}, ax=ax)
+    ax.set_title(f'Relation entre {var} et _SDQ_TotaleProblematics')
+    sns.scatterplot(x='_SDQ_TotaleProblematics', y=var, data=datasoinorma, color=color, ax=ax)
+
+# analyse de corrélation  de pearson
+x= datasoinorma[['MALE', 'AGE_p.6', 'scoreTotalConflit','scoreTotalAnxiety']] # les prédicteurs
+
+y=datasoinorma.scoreTotalImageSoi
+
+corr, p_value=pearsonr(x,y)
+print(f"Corrélation de Pearson :, coor")
+print(f"P_value ;, p_value")
